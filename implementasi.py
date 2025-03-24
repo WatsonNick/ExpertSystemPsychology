be0efa363dda380e21ea0bd14f3fc41d805dfe5a
WatsonNick <110089525+WatsonNick@users.noreply.github.com>
2025-03-24 19:38:40 +0700

    Update implementasi.py

--- implementasi.py
+++ implementasi.py
@@ -84,7 +84,7 @@ if selected == "Halaman Utama":
                             background-color: #f9f9f9;
                             overflow: hidden;
                             line-height: 1;">
-                            <a style="margin: 0;">{i + 1}. {statement}</a>
+                            <p style="margin: 0;">{i + 1}. {statement}</p>
                         </div>
                         """, 
                         unsafe_allow_html=True
